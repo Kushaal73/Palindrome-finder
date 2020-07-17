4900d808e135d7a6edf09b682a1ca6628b9aab14
Kushaal73 <68424040+Kushaal73@users.noreply.github.com>
2020-07-17 07:07:10 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let word = prompt('whats the word');
+let length = word.split().length;
+let split = word.split('');
+console.log('word length is',word.length);
+let lastIndex = split[length-1];
+console.log(lastIndex);
